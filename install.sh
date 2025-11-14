deec80202c163856469ab412610f4288c039b4dc
taylordolletzki <djqm7pmn8w@privaterelay.appleid.com>
2025-11-13 23:57:42 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ set -e
 echo "Installing Grok Direct P2P v0.2.1"
 python3 -m venv venv
 source venv/bin/activate
-pip install "qrcode[pil]" pillow
+pip install "qrcode[pil]" requests
 echo "Done! Run: source venv/bin/activate && python node/grok_node.py"
